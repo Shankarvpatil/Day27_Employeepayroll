008b55497764c0fd7b4250ea93304306ed234f3a
Bhandari <Bhandari@DESKTOP-VD7FK5G>
2021-07-26 03:55:50 -0700

    Employee Pay Roll

---
+++ src/com/bridgelabz/program/EmployeePayrollTest.java
@@ -0,0 +1,53 @@
+package com.bridgelabz.program;
+
+import static org.junit.Assert.assertEquals;
+import java.lang.AssertionError;
+import org.junit.Test;
+
+public class EmployeePayrollTest 
+{
+	@Test
+	public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries() 
+	{
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		employeePayrollService.addEmployee(new EmployeeData(1,"Jeff Bezos",10000));
+		employeePayrollService.addEmployee(new EmployeeData(2,"Bill Gates",20000));
+		employeePayrollService.addEmployee(new EmployeeData(3,"Mark Z",30000));
+		employeePayrollService.writeEmployeeDataToFile();
+		long entries = employeePayrollService.countEntries();
+		assertEquals(3, entries);
+	}
+	
+	@Test
+	public void given3EmployeesWhenWrittenToFileShouldPrintEmployeeEntries() 
+	{
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		employeePayrollService.addEmployee(new EmployeeData(1,"Jeff Bezos",10000));
+		employeePayrollService.addEmployee(new EmployeeData(2,"Bill Gates",20000));
+		employeePayrollService.addEmployee(new EmployeeData(3,"Mark Z",30000));
+		employeePayrollService.writeEmployeeDataToFile();
+		employeePayrollService.printData();
+		long entries = employeePayrollService.countEntries();
+		assertEquals(3, entries);
+	}
+
+	@Test
+	public void given3EmployeesWhenWrittenToFileShouldPrintNumberOfEmployeeEntries() 
+	{
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		employeePayrollService.addEmployee(new EmployeeData(1,"Jeff Bezos",10000));
+		employeePayrollService.addEmployee(new EmployeeData(2,"Bill Gates",20000));
+		employeePayrollService.addEmployee(new EmployeeData(3,"Mark Z",30000));
+		employeePayrollService.writeEmployeeDataToFile();
+		long entries = employeePayrollService.countEntries();
+		System.out.println("Number of entries are "+entries);
+		assertEquals(3, entries);
+	}
+	@Test
+	public void givenFileOnReadindFromFileShouldMatchEmployeeCount() 
+	{
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		long entries = employeePayrollService.readDataFromFile();
+		assertEquals(3, entries);
+	}
+}
